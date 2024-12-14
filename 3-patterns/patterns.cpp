a536647be9b0f52495711ea3b40dd6ffe2cd09b8
Usman khan <musmankhan707@gmail.com>
2024-12-14 12:12:20 +0500

    patterns 5

--- 3-patterns/patterns.cpp
+++ 3-patterns/patterns.cpp
@@ -1,46 +1,102 @@
 #include <iostream>
 using namespace std;
 
-int main(){
-    // 0 0 0 0 
+int main()
+{
+    cout << endl;
+    cout << "Pattern 1" << endl;
+    cout << endl;
+    // 0 0 0 0
     // 1 1 1 1
     // 2 2 2 2
     // 3 3 3 3
     // 4 4 4 4
-    for(int i=0; i<=4; i++){
-        cout<<i<<" "<<i<<" "<<i<<" "<<i<<" "<<endl;;        
+    for (int i = 0; i <= 4; i++)
+    {
+        cout << i << " " << i << " " << i << " " << i << " " << endl;
+        ;
     }
-    cout<<endl;
+    cout << endl;
+    cout << "Pattern 2" << endl;
+    cout << endl;
     // *
     // * *
     // * * *
     // * * * *
-    for(int i=0; i<=3;i++){
+    for (int i = 0; i <= 3; i++)
+    {
         char staric = '*';
         int j = -1;
-        while(i !=j){
-            cout<<staric<<" ";
+        while (i != j)
+        {
+            cout << staric << " ";
             j++;
         }
-        cout<<endl;
+        cout << endl;
     }
-    cout<<endl;
+    cout << endl;
+    cout << "Pattern 3" << endl;
+    cout << endl;
     // * * * *
     // * * *
-    // * * 
+    // * *
     // *
-    for(int i=4; i!=0;i--){
+    for (int i = 4; i != 0; i--)
+    {
         char staric = '*';
         int j = 0;
-        while(i != j){
-            cout<<staric<<" ";
+        while (i != j)
+        {
+            cout << staric << " ";
             j++;
         }
-        cout<<endl;
+        cout << endl;
     }
-    cout<<endl;
-    // * * * *
-    // * * *
-    // * * 
-    // *
+    cout << endl;
+    cout << "Pattern 4" << endl;
+    cout << endl;
+    // 1
+    // 12
+    // 123
+    // 1234
+    for (int i = 0; i <= 4; i++)
+    {
+        // char staric = '*';
+        int j = 0;
+        while (i != j)
+        {
+            j++;
+            cout << j;
+        }
+        cout << endl;
+    }
+    cout << endl;
+    cout << "Pattern 5" << endl;
+    cout << endl;
+    // A
+    // BC
+    // DEF
+    // GHIJ
+    int j = 0;
+    for (int i = 0; i <= 3; i++)
+    {
+        char arr[] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'};
+
+        int loop_count = -1;
+        while (loop_count != i)
+        {
+            cout << arr[j];
+            j++;
+            loop_count++;
+        }
+        cout << endl;
+    }
+
+    cout << endl;
+    cout << "Pattern 5" << endl;
+    cout << endl;
+    // A
+    // BC
+    // DEF
+    // GHIJ
 }
