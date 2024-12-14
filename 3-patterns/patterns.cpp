61e6bf43234c7ef5b23350361e15e55c1221ef37
Usman khan <musmankhan707@gmail.com>
2024-12-14 12:59:08 +0500

    pattern 7

--- 3-patterns/patterns.cpp
+++ 3-patterns/patterns.cpp
@@ -99,7 +99,7 @@ int main()
     // *   *
     // *   *
     // *****
-    
+
     for (int i = 0; i < 4; i++)
     {
         char staric = '*';
@@ -113,6 +113,31 @@ int main()
             cout << " " << " " << " ";
         }
         cout << staric;
+        cout << endl;
+    }
+    cout << endl;
+    cout << "Pattern 7" << endl;
+    cout << endl;
+    //     *
+    //    **
+    //   ***
+    //  ****
+
+    for (int i = 0; i < 4; i++)
+    {
+        char staric = '*';
+        j = 3 - i;
+        while (j != 0)
+        {
+            cout << " ";
+            j--;
+        }
+        int z = -1;
+        while (z != i)
+        {
+            cout << staric;
+            z++;
+        }
         cout<<endl;
     }
 }
