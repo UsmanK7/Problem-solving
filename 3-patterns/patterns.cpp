1916e3865453969700a0e0c064116bfced6fb6df
Usman khan <musmankhan707@gmail.com>
2024-12-14 13:00:13 +0500

    pattern 8

--- 3-patterns/patterns.cpp
+++ 3-patterns/patterns.cpp
@@ -140,4 +140,13 @@ int main()
         }
         cout<<endl;
     }
+
+    cout << endl;
+    cout << "Pattern 8" << endl;
+    cout << endl;
+    //  1
+    //  2  3
+    //  4  5  6
+    //  7  8  9  10
+    //  11 12 13 14
 }
