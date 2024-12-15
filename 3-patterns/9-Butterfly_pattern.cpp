2ad6ac66e995d9e311eb66b058a2a9657536385f
Usman khan <musmankhan707@gmail.com>
2024-12-15 18:57:04 +0500

    butterfly

--- 3-patterns/9-Butterfly_pattern.cpp
+++ 3-patterns/9-Butterfly_pattern.cpp
@@ -16,5 +16,30 @@ int main()
     //  *      *
     char staric = '*';
     int n =4;
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=i;j++){
+            cout<<staric;
+        }
+        for(int j=1;j<=2*(n-i);j++){
+            cout<<" ";
+        }
+        for(int j=1;j<=i;j++){
+            cout<<staric;
+        }
+        cout<<endl;
+    }
+    for(int i=n;i>=n;i--){
+        for(int j=1;j<=i;j++){
+            cout<<staric;
+        }
+        for(int j=1;j<=2*(n-i);j++){
+            cout<<" ";
+        }
+        for(int j=1;j<=i;j++){
+            cout<<staric;
+        }
+        cout<<endl;
+    }
 
+    
 }
