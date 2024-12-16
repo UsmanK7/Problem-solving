1d01fb6e05081520671af726c30fe3877628ed17
Usman khan <musmankhan707@gmail.com>
2024-12-16 11:00:54 +0500

    pattern

--- 3-patterns/patterns.cpp
+++ 3-patterns/patterns.cpp
@@ -3,19 +3,7 @@ using namespace std;
 
 int main()
 {
-    cout << endl;
-    cout << "Pattern 1" << endl;
-    cout << endl;
-    // 0 0 0 0
-    // 1 1 1 1
-    // 2 2 2 2
-    // 3 3 3 3
-    // 4 4 4 4
-    for (int i = 0; i <= 4; i++)
-    {
-        cout << i << " " << i << " " << i << " " << i << " " << endl;
-        ;
-    }
+   
     cout << endl;
     cout << "Pattern 2" << endl;
     cout << endl;
